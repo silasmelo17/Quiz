54419c024ce1066a5498768d6b6efec90a1d990a
silasmelo17 <silasmelo1967@gmail.com>
2021-07-10 17:19:11 -0300

    create page

---
+++ pages/register.tsx
@@ -0,0 +1,16 @@
+
+import Head from 'next/head'
+
+import Register from '@Components/Register'
+
+
+
+export default function RegisterQuiz () {
+
+    return(<>
+        <Head>
+            <title>Register quiz</title>
+        </Head>
+        <Register />
+    </>)
+}
